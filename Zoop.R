bf8573bf1121a75c2ccd14b0382af72c20214a71
mmmorrissey <153395414+mmmorrissey@users.noreply.github.com>
2023-12-16 14:20:26 -0800

    Update Zoop.R

--- Zoop.R
+++ Zoop.R
@@ -4,39 +4,30 @@ head(Zoop_SIA)
 colnames(Zoop_SIA)
 colnames(Zoop_SIA)[5]<-"Distance"
 Zoop_SIA["Distance"]
-Zoop_SIA$Order[Zoop_SIA$Order == "Calanoid"] <- "Calanoida"
-
-
 library(ggplot2)
-#Wunderbar. Subset the orders
-Calanoid_SIA<-subset(Zoop_SIA,Order=="Calanoid")
+#Subset the orders
+Calanoida_SIA<-subset(Zoop_SIA,Order=="Calanoida")
 Cladocera_SIA<-subset(Zoop_SIA,Order=="Cladocera")
-#perhaps we should add distance from colony
-#ok added. now see if there's relationship between 
-#distance from colony & d15N
-#-----------------------------------
 #d15N
 #create LM model, check residuals & fit
-##Calanoid lm
-Calanoid_lm<-lm(d15N~Distance,data=Calanoid_SIA)
-plot(Calanoid_lm) #Pond 1 high leverage point
-resNoid<-resid(Calanoid_mod)
-fitNoid<-fitted(Calanoid_mod)
+##Calanoida lm
+Calanoida_lm<-lm(d15N~Distance,data=Calanoida_SIA)
+plot(Calanoida_lm) #Pond 1 high leverage point
+resNoid<-resid(Calanoida_mod)
+fitNoid<-fitted(Calanoida_mod)
 plot(fitNoid,resNoid,ylab="Residuals",
      xlab="Fitted values")
 hist(resNoid,breaks=5)
-lag.plot(resNoid, main="Lag plot, Calanoid d15N residuals",
+lag.plot(resNoid, main="Lag plot, Calanoida d15N residuals",
          diag=FALSE,do.lines=FALSE)
 qqnorm(resNoid)
-summary.lm(Calanoid_mod)
+summary.lm(Calanoida_mod)
 #0.05844
-library(ggplot2)
-ggplot(data=Calanoid_SIA,aes(x=Distance,y=d15N)) + 
+ggplot(data=Calanoida_SIA,aes(x=Distance,y=d15N)) + 
   geom_point() + 
   theme_bw() + 
-  labs(title="Calanoid δ15N",x="Distance from colony (km)",
+  labs(title="Calanoida δ15N",x="Distance from colony (km)",
       y="δ15N (‰)")
-
 ##Cladocera lm
 Cladocera_lm<-lm(d15N~Distance,data=Cladocera_SIA)
 plot(Cladocera_lm) #all points good
@@ -56,9 +47,8 @@ ggplot(data=Cladocera_SIA,aes(x=Distance,y=d15N))+
   theme_bw() + 
   labs(title="Cladocera δ15N",x="Distance from colony (km)",
        y="δ15N (‰)")
-
 ##both Clad and Cala look like they could be exponential fits
-exp.noid<-lm(formula = log(d15N)~Distance,data=Calanoid_SIA)
+exp.noid<-lm(formula = log(d15N)~Distance,data=Calanoida_SIA)
 exp.cera<-lm(formula = log(d15N) ~ Distance,data=Cladocera_SIA)
 summary(exp.noid)
 #r2 = 0.600, p = 0.02397
@@ -67,7 +57,7 @@ plot(exp.cera)
 summary(exp.cera) #pond 12 high leverage point
 summary(exp.cera)
 #r2 = 0.3857, p = 0.0414
-ggplot(data=Calanoid_SIA,aes(x=Distance,y=d15N)) + 
+ggplot(data=Calanoida_SIA,aes(x=Distance,y=d15N)) + 
   geom_point() + 
   geom_smooth(method="nls",
               formula=y ~ a*exp(-b *x),
@@ -76,20 +66,14 @@ ggplot(data=Calanoid_SIA,aes(x=Distance,y=d15N)) +
               colour="red") + 
   labs(x="Distance from colony (km)") +
   ylab(bquote(δ^15*"N(‰)"))
-#changing the 'a' and 'b' values doesn't change the 
-#line at all, weird..
-#y-intercept should be different :(
-exp(9.4811)
-
 #maybe logarithmic is better?
-exp.noid.2<-lm(formula=d15N~log(Distance),data=Calanoid_SIA)
+exp.noid.2<-lm(formula=d15N~log(Distance),data=Calanoida_SIA)
 plot(exp.noid.2) #Pond 1- high leverage point
 summary(exp.noid.2) #r2 = 0.8446, p = 0.0012
 exp.cera.2<-lm(formula=d15N~log(Distance),data=Cladocera_SIA)
 plot(exp.cera.2) ##still issues- Pond 1 high leverage point
 summary(exp.cera.2) #r2 = 0.6758, p = 0.0010
 #but they look great!
-
 ggplot(Cladocera_SIA,aes(x=Distance,y=d15N)) + 
   geom_point()+ 
   theme_bw() +
@@ -99,10 +83,7 @@ ggplot(Cladocera_SIA,aes(x=Distance,y=d15N)) +
   labs(x="Distance from colony (km)") +
   ylab(bquote(δ^15*"N(‰)"))
 #smooth formula is: y = 4.1382 -2.5275ln(x)
-library(SciViews)
-4.1382-2.5275*ln(0.226)
-#correct answer!
-ggplot(data=Calanoid_SIA,aes(x=Distance,y=d15N)) + 
+ggplot(data=Calanoida_SIA,aes(x=Distance,y=d15N)) + 
   geom_point() + 
   theme_bw() +
   geom_smooth(method="lm",
@@ -113,24 +94,24 @@ ggplot(data=Calanoid_SIA,aes(x=Distance,y=d15N)) +
 #formula: y = 8.8421-3.1038*ln(x)
 8.8421-3.1038*ln(0.226)
 #yay!
-#cladocera and calanoid
+#cladocera and calanoida
 ggplot(Zoop_SIA,aes(x=Distance,y=d15N,colour=Order)) + 
   geom_point() + 
   theme_bw() +
   labs(x="Distance from colony (km)") +
   ylab(bquote(δ^15*"N(‰)"))
 #correlation between two
-cor.test(Calanoid_SIA$d15N,Cladocera_SIA$d15N)
+cor.test(Calanoida_SIA$d15N,Cladocera_SIA$d15N)
 #problem because there are different numbers of 
 #samples in the two orders.
 #so let's make a df with only the 8 ponds with values
 head(Zoop_SIA)
 head(Cladocera_SIA)
 tail(Cladocera_SIA)
-head(Calanoid_SIA)
-Calanoid_SIA_2<-Calanoid_SIA
-colnames(Calanoid_SIA_2)[4]<-"d15N.Calanoid"
-head(Calanoid_SIA_2)
+head(Calanoida_SIA)
+Calanoida_SIA_2<-Calanoida_SIA
+colnames(Calanoida_SIA_2)[4]<-"d15N.Calanoida"
+head(Calanoida_SIA_2)
 Cladocera_SIA$d15N
 Cladocera_d15N<-Cladocera_SIA$d15N
 Cladocera_d15N
@@ -138,11 +119,11 @@ Cladocera_d15N
 Cladocera_d15N_remove<-Cladocera_d15N[-c(2,3,4,7)]
 Cladocera_d15N_remove
 #yee
-Zoop_d15N_all<-cbind(Calanoid_SIA_2,Cladocera_d15N_remove)
+Zoop_d15N_all<-cbind(Calanoida_SIA_2,Cladocera_d15N_remove)
 head(Zoop_d15N_all)
 colnames(Zoop_d15N_all)[6]<-"d15N.Cladocera"
 colnames(Zoop_d15N_all)
-cor.test(Zoop_d15N_all$d15N.Calanoid,Zoop_d15N_all$d15N.Cladocera)
+cor.test(Zoop_d15N_all$d15N.Calanoida,Zoop_d15N_all$d15N.Cladocera)
 #---------------------------------------------------
 #d13C
 #visualize first
@@ -151,9 +132,9 @@ ggplot(Zoop_SIA,aes(x=Distance,y=d13C,color=Order)) +
   geom_smooth(method="lm")
 #looks maybe parabolic?
 #let's try lm first
-Calanoid_C_lm<-lm(d13C~Distance,data=Calanoid_SIA)
-plot(Calanoid_C_lm) #no leverage points
-summary(Calanoid_C_lm) #p = 0.459, r2 = 0.09
+Calanoida_C_lm<-lm(d13C~Distance,data=Calanoida_SIA)
+plot(Calanoida_C_lm) #no leverage points
+summary(Calanoida_C_lm) #p = 0.459, r2 = 0.09
 Cladocera_C_lm<-lm(d13C~Distance,data=Cladocera_SIA)
 plot(Cladocera_C_lm) #no strong leverage points
 summary(Cladocera_C_lm) #p = 0.629, r2 = 0.024
@@ -162,9 +143,9 @@ ggplot(Cladocera_SIA,aes(x=Distance,y=d13C)) +
   geom_point() + 
   geom_smooth(method="lm")
 #polynomial models
-Calanoid_poly_C<-lm(d13C~poly(Distance,degree=2,raw=T),data=Calanoid_SIA)
-summary(Calanoid_poly_C) #r2 = 0.401, p=0.278
-ggplot(Calanoid_SIA,aes(x=Distance,y=d13C)) + 
+Calanoida_poly_C<-lm(d13C~poly(Distance,degree=2,raw=T),data=Calanoida_SIA)
+summary(Calanoida_poly_C) #r2 = 0.401, p=0.278
+ggplot(Calanoida_SIA,aes(x=Distance,y=d13C)) + 
   geom_point() + 
   theme_bw() +
   geom_smooth(method="lm",
@@ -183,7 +164,7 @@ ggplot(Cladocera_SIA,aes(x=Distance,y=d13C)) +
 
 #correlation between N and C?
 cor.test(Cladocera_SIA$d13C,Cladocera_SIA$d15N)#p=0.41,r=0.26
-cor.test(Calanoid_SIA$d13C,Calanoid_SIA$d15N) #p=0.12,r=0.59
+cor.test(Calanoida_SIA$d13C,Calanoida_SIA$d15N) #p=0.12,r=0.59
 #not really
 
 #---------------------------------------------------
@@ -197,7 +178,7 @@ library(gclus)
 library(ape)
 library(cluster)
 CSM_total<-cbind(Zoop_SIA,Pond_data)
-#too many rows in the Zoop SIA df, because Calanoid and
+#too many rows in the Zoop SIA df, because Calanoida and
 #Cladocera separated. Make them separate
 Cladocera_Pond<-cbind(Pond_data,Cladocera_SIA)
 head(Cladocera_Pond)
@@ -219,25 +200,25 @@ ggplot(Zoop_SIA,aes(x=Distance,y=d13C,color=Order)) +
   geom_point()
 Cladocera_d13C_lm<-lm(d13C~Distance,data=Cladocera_SIA)
 summary(Cladocera_d13C_lm)
-Calanoid_d13C_lm<-lm(d13C~Distance,data=Calanoid_SIA)
-summary(Calanoid_d13C_lm)
+Calanoida_d13C_lm<-lm(d13C~Distance,data=Calanoida_SIA)
+summary(Calanoida_d13C_lm)
 
 
 #---------------------------------------------------------
 #Removing "high leverage" points for d15N
 Cladocera_remove<-Cladocera_SIA[-c(1),]
-Calanoid_remove<-Calanoid_SIA[-c(1),]
+Calanoida_remove<-Calanoida_SIA[-c(1),]
 Cladocera_remove_log<-lm(formula=d15N~log(Distance),data=Cladocera_remove)
-Calanoid_remove_log<-lm(formula=d15N~log(Distance),data=Calanoid_remove)
+Calanoida_remove_log<-lm(formula=d15N~log(Distance),data=Calanoida_remove)
 plot(Cladocera_remove_log)
-plot(Calanoid_remove_log)
+plot(Calanoida_remove_log)
 #so we got rid of the high leverage points.
-summary(Calanoid_remove_log)
+summary(Calanoida_remove_log)
 #no longer significant, although r2 is 0.5063
 summary(Cladocera_remove_log)
 #still significant, but not as much
 
-ggplot(Calanoid_remove,aes(x=Distance,y=d15N)) + 
+ggplot(Calanoida_remove,aes(x=Distance,y=d15N)) + 
   geom_point() + 
   geom_smooth(method="lm",
               formula=y~log(x),
